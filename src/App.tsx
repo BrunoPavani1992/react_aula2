d5f6e1b096e9f0e8d88be291d43ce8bdd71a7886
Bruno Pavani <brunopavani1992@gmail.com>
2022-05-01 20:59:22 -0300

    Finishing the course

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard"
 import { GlobalStyle } from "./styles/globals";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { useState } from 'react';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
     }
 
     return (
-        <>
+        <TransactionsProvider>
             <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
             <NewTransactionModal 
@@ -27,6 +28,6 @@ export function App() {
                 onRequestClose={handleCloseNewTransactionModal}
             />
             <GlobalStyle />
-        </>
+        </TransactionsProvider>
   );
 }
